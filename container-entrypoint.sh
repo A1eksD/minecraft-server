d4d23cc265f2dee8dc3f861e230f2f3b37d42911
Aleks <143895611+A1eksD@users.noreply.github.com>
2025-08-29 21:13:12 +0200

    3

--- container-entrypoint.sh
+++ container-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-# EULA automatisch akzeptieren (optional)
+# EULA automatisch akzeptieren
 [ "$EULA" = "TRUE" ] && [ ! -f eula.txt ] && echo "eula=true" > eula.txt
 
+# ---- Neu: Server in den Offline-Modus schalten --------------------------
+# Falls Zeile schon existiert → ersetzen, sonst anhängen
+grep -q '^online-mode=' server.properties \
+  && sed -i 's/^online-mode=.*/online-mode=false/' server.properties \
+  || echo 'online-mode=false' >> server.properties
+# ------------------------------------------------------------------------
+
+# Server starten
 java -Xmx1024M -Xms1024M -jar /app/server.jar nogui
